bfc4ad7dfc54de7152a3e8d3cab861b2b273d0da
Malek Farag <faragmalek14@gmail.com>
2023-11-18 15:50:06 -0500

    added branding

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,15 @@ import QuizHistory from '../app/components/QuizHistory';
 
 export default function Home() {
   return (
-    <main className="max-w-[900px] mx-auto py-24 px-2 md:px-24">
+    <main className="max-w-[900px] mx-auto pb-24 px-2 md:px-24">
+
+    <a  href='https://arlinear.com/' 
+        target='_blank' 
+        className='block my-14'
+    >
+      <img className='w-24 md:w-36 mx-auto' src='/arlinear-logo-wordmark.png' />
+      <h1 className='text-center mt-2'>Quiz API Demo</h1>
+    </a>
 
     <div className="flex flex-col items-center gap-3">
       <GenerateQuizForm />
